08de1c0df892fec6bb6e75db3391a76dfeebe40d
zako-xx1 <zakwanafzal2216>
2025-10-29 03:12:54 +0500

    new project

---
+++ js/script.js
@@ -0,0 +1,76 @@
+
+const checkBoxList = document.querySelectorAll('.custom-checkbox')
+const inputFields = document.querySelectorAll('.goal-input')
+const errorLabel = document.querySelector('.error-label')
+const progressBar = document.querySelector('.progress-bar')
+const progressValue = document.querySelector('.progress-value')
+
+
+//get tiem into local storage
+let allGoals = JSON.parse(localStorage.getItem('todayGoals')) || {};
+//check how many complete
+let completedGoalsCount = Object.values(allGoals).filter(
+  (goal) => goal.completed
+).length
+progressValue.style.width = `${completedGoalsCount / 3 * 100}%`;
+progressValue.firstElementChild.textContent = `${completedGoalsCount}/3 completed`;
+
+// loop in array  to  get value checkbox
+checkBoxList.forEach((checkbox) => {
+  //check all input are full not empty
+  checkbox.addEventListener('click', (e) => {
+    const allGoalsAdded = [...inputFields].every((input) => {
+      return input.value.trim() !== ''; // check input field is not empty
+    })
+    //toggle add class 
+    if (allGoalsAdded) {
+      checkbox.parentElement.classList.toggle('completed');
+      errorLabel.style.display = 'none';
+      //checkbox in local storage
+      const inputID = checkbox.nextElementSibling.id;
+      allGoals[inputID].completed = !allGoals[inputID].completed;
+      completedGoalsCount = Object.values(allGoals).filter(
+        (goal) => goal.completed
+      ).length;
+      progressValue.style.width = `${completedGoalsCount / 3 * 100}%`;
+      progressValue.firstElementChild.textContent = `${completedGoalsCount}/3 completed`;
+      localStorage.setItem('todayGoals', JSON.stringify(allGoals));
+    } else {
+      errorLabel.style.display = 'block';
+    }
+  })
+})
+//arraay looop in input to get one by one input
+inputFields.forEach((input) => {
+  // local to ui input value
+  input.value = allGoals[input.id].goal;
+  if (allGoals[input.id].completed) {
+    input.parentElement.classList.add('completed')
+  }
+  //if input is focus then error is hide
+  input.addEventListener('focus', () => {
+    errorLabel.style.display = 'none';
+
+  })
+  //if local storage have input value
+  input.addEventListener('input', (e) => {
+if (allGoals[input.id].completed) {
+  input.value = allGoals[input.id].goal;
+  input.setAttribute('readonly', true);
+  input.style.cursor = "url('../images/cursor.svg'), not-allowed";
+}
+
+    allGoals[input.id] = {
+      goal: input.value,
+      completed: false
+    }
+    // setitem into localStorage
+    localStorage.setItem('todayGoals', JSON.stringify(allGoals));
+  });
+});
+
+
+
+
+
+
